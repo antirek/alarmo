b1a3284ee35bbac9b56d350bf16975abc9bb6c6d
Sergey Dmitriev <serge.dmitriev@gmail.com>
2018-09-26 17:26:04 +0700

    fix changes

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const config = require('config')
 const Telegraf = require('telegraf')
 
 const User = require('./lib/user')
-let telegrafApp = new Telegraf(config.token)
-
-let TelegramServer = require('./lib/telegram')
 
 let Messages = {
   hello: 'Добрый день, %s!\nНажмите кнопку "Отправить номер" внизу, чтобы начать.',
@@ -15,12 +12,29 @@ let Messages = {
   notyournumber: 'Вероятно, это не ваш номер :('
 }
 
-let telegram = new TelegramServer(telegrafApp, User, Messages)
+let telegram, viber;
 
-telegram.telegrafApp.startPolling()
+if (config.telegram) {
+    let telegrafApp = new Telegraf(config.telegram.token)
+    let TelegramServer = require('./lib/telegram/telegram')
 
-let HttpServer = require('./lib/http')
-let http = new HttpServer(telegram.telegrafApp, User, config.auth)
+    telegram = new TelegramServer(telegrafApp, User, Messages)
+    telegram.telegrafApp.startPolling()
+}
+
+if (config.viber) {
+    let ViberBot = require('./lib/viber/viber');
+    viber = new ViberBot(User);
+}
+
+let HttpServer = require('./lib/http');
+
+let sender = {
+    telegramApp: config.telegram ? telegram.telegrafApp : null,
+    viber: config.viber ? viber.bot : null
+};
+
+let http = new HttpServer(sender, User, config.auth)
 
 http.expressApp.listen(config.port, () => {
   console.log('http started on port', config.port)
